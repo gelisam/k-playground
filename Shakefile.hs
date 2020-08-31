9e8e3abeb923f374a05b866736e01b3fe5823766
Samuel Gélineau <gelisam+github@gmail.com>
2020-08-31 09:14:17 -0400

    ./build.sh tests

--- Shakefile.hs
+++ Shakefile.hs
@@ -34,13 +34,34 @@ rulesFileToRulesFolder :: FilePath -> FilePath
 rulesFileToRulesFolder = takeDirectory
 
 -- |
--- >>> actualResultToRulesFolder "src/operational-semantics/small-steps/example.actual"
+-- >>> actualResultToRulesFolder "src/operational-semantics/small-steps/foo.actual"
 -- "src/operational-semantics/small-steps"
 actualResultToRulesFolder :: FilePath -> FilePath
 actualResultToRulesFolder = takeDirectory
 
+-- |
+-- >>> actualResultToExampleFile "src/operational-semantics/small-steps/foo.actual"
+-- "src/examples/foo.lambda"
 actualResultToExampleFile :: FilePath -> FilePath
 actualResultToExampleFile actualFile = "src" </> "examples" </> takeBaseName actualFile <.> "lambda"
+
+-- |
+-- >>> passedProofToExpectedFile "src/operational-semantics/small-steps/foo.passed"
+-- "src/operational-semantics/small-steps/foo.actual"
+passedProofToActualFile :: FilePath -> FilePath
+passedProofToActualFile passedFile = passedFile -<.> "actual"
+
+-- |
+-- >>> passedProofToExpectedFile "src/operational-semantics/small-steps/foo.passed"
+-- "src/operational-semantics/small-steps/foo.expected"
+passedProofToExpectedFile :: FilePath -> FilePath
+passedProofToExpectedFile passedFile = passedFile -<.> "expected"
+
+-- |
+-- >>> passedProofToExpectedFile "src/operational-semantics/small-steps/foo.expected"
+-- "src/operational-semantics/small-steps/foo.passed"
+expectedFileToPassedProof :: FilePath -> FilePath
+expectedFileToPassedProof passedFile = passedFile -<.> "passed"
 
 -- |
 -- >>> dockerizePath "src/foo/bar/baz.txt"
@@ -53,16 +74,19 @@ main :: IO ()
 main = do
   pwd <- getCurrentDirectory
   let dockerFlags = ["--workdir=/root", "--mount", "type=bind,source=" <> pwd </> "src,target=/root"]
-  let dockerCmd cmd = command [EchoStdout True, EchoStderr True]
-                               "docker" $ ["run"] <> dockerFlags <> [dockerImage] <> cmd
+  let dockerCmd cmd = command [] "docker" $ ["run"] <> dockerFlags <> [dockerImage] <> cmd
   let dockerCmd_ = void . dockerCmd_
 
   rulesFiles <- getDirectoryFilesIO "" [rulesFolderToRulesFile "src//"]
   let rulesFolders = map rulesFileToRulesFolder rulesFiles
   let rulesProofs = map rulesFolderToRulesProof rulesFolders
 
+  expectedFiles <- getDirectoryFilesIO "" ["src//*.expected"]
+  let passedProofs = map expectedFileToPassedProof expectedFiles
+  let actualFiles = map passedProofToActualFile passedProofs
+
   shakeArgs shakeOptions $ do
-    want ["rules"]
+    want ["tests"]
 
     phony "repl" $ do
       -- '-it' commands must be run from a real terminal
@@ -89,3 +113,19 @@ main = do
       need [rulesProof]
       Stdout out <- dockerCmd ["krun", "--directory", dockerizePath rulesFolder, dockerizePath exampleFile]
       liftIO $ writeFile actualResult out
+
+    phony "tests" $ do
+      need passedProofs
+      putInfo "*** TESTS PASSED ***"
+
+    for_ actualFiles $ \actualFile -> do
+      phony (dropExtensions . dockerizePath $ actualFile) $ do
+        need [actualFile]
+        cmd_ "cat" actualFile
+
+    "src//*.passed" %> \passedProof -> do
+      let expectedFile = passedProofToExpectedFile passedProof
+      let actualFile = passedProofToActualFile passedProof
+      need [expectedFile, actualFile]
+      command_ [] "diff" ["-urN", expectedFile, actualFile]
+      liftIO $ writeFile passedProof ""
